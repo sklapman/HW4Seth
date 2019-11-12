99ac824baa2f0b6afbe24e95efa934d703ba5705
sklapman <klapman.seth@gmail.com>
2019-11-12 09:09:06 -0500

    seth test 2

--- app/src/main/java/com/example/hw4/MainActivity.java
+++ app/src/main/java/com/example/hw4/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Seth Test addition
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
